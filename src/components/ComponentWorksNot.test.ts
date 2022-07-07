7ad0712b6a7f7c2650fbba8f92878a395f3e374a
sand4rt <sander@divotion.com>
2022-07-07 15:46:04 +0200

    formatting

--- src/components/ComponentWorksNot.test.ts
+++ src/components/ComponentWorksNot.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/experimental-ct-vue';
 import ComponentWorksNot from './ComponentWorksNot.vue';
 
 test('renders a link', async ({ mount }) => {
-	const component = await mount(ComponentWorksNot);
+    const component = await mount(ComponentWorksNot);
 
     await expect(component).toHaveText('test');
 });
