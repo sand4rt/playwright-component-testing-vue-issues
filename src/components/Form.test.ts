1205d1ecfe1a5be9212e71bdc3205360aa9c743c
sand4rt <sander@divotion.com>
2022-07-24 21:43:43 +0200

    testing emits

---
+++ src/components/Form.test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/experimental-ct-vue';
+import Form from './Form.vue';
+
+test('emit a submit event when the button is clicked', async ({ mount }) => {
+    const component = await mount(Form);
+
+    await component.locator('button').click();
+
+	await expect(component.emitted().submit).toMatchObject({ username: 'admin' })
+});
---
+++ src/components/Form.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const emit = defineEmits(['submit'])
+
+function onSubmit() {
+    emit('submit', { username: 'admin' });
+}
+</script>
+<template>
+ <form @submit="onSubmit">
+    <button type="submit">Submit</button>
+ </form>
+</template>
