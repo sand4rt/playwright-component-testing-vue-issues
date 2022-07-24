fae7008b7f71f6d77ebd075d72ae796b76e722e8
sand4rt <info@mesander.com>
2022-07-24 22:05:41 +0200

    Update Form.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
 const emit = defineEmits(['submit'])
 
+const form = ref({ username: 'admin' });
+
 function onSubmit() {
-    emit('submit', { username: 'admin' });
+    emit('submit', form.value);
 }
 </script>
 <template>
- <form @submit="onSubmit">
+ <form @submit.prevent="onSubmit">
+    <input type="text" v-model="form.username" name="username" />
     <button type="submit">Submit</button>
  </form>
 </template>
